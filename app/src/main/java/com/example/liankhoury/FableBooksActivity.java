a8cf72c2e2594a216bd5f1c3f9e4cce8696744fd
LianKhoury <khourylian1@gmail.com>
2022-11-02 13:27:05 +0200

    ss

---
+++ app/src/main/java/com/example/liankhoury/FableBooksActivity.java
@@ -0,0 +1,14 @@
+package com.example.liankhoury;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FableBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fable_books);
+    }
+}
---
+++ app/src/main/java/com/example/liankhoury/FairytaleBooksActivity.java
@@ -0,0 +1,14 @@
+package com.example.liankhoury;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FairytaleBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fairytale_books);
+    }
+}
