ae213f113bbf474e518d4e745b36b86e48ed4026
LianKhoury <khourylian1@gmail.com>
2023-02-06 09:55:15 +0200

    ss

--- app/src/main/java/com/example/liankhoury/CameraActivity.java
+++ app/src/main/java/com/example/liankhoury/CameraActivity.java
@@ -3,8 +3,25 @@ package com.example.liankhoury;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.Button;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private static final String TAG = "AndroidCameraApi";
+    private Button btn_Take;
+    private Button btn_Gallery;
+    private TextureView textureView;
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0,90);
+        ORIENTATIONS.append(Surface.ROTATION_90,0);
+        ORIENTATIONS.append(Surface.ROTATION_180,270);
+        ORIENTATIONS.append(Surface.ROTATION_270,180);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
