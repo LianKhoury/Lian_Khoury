be476720b423800d2de17b60ad7df2eeade43120
LianKhoury <khourylian1@gmail.com>
2023-02-22 14:21:58 +0200

    ss

--- app/src/main/java/com/example/liankhoury/UserProfileActivity.java
+++ app/src/main/java/com/example/liankhoury/UserProfileActivity.java
@@ -3,12 +3,54 @@ package com.example.liankhoury;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    private TextView TextViewProfile,T_VShowFullName,T_VShowEmail,T_VShowPhoneNum;
+    private ProgressBar progressBar;
+    private String FullName,email,phone;
+    private ImageView imageView;
+    private FirebaseAuth authProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        getSupportActionBar().setTitle("Home");
+
+        TextViewProfile = findViewById(R.id.TextViewProfile);
+        T_VShowFullName = findViewById(R.id.T_VShowFullName);
+        T_VShowEmail = findViewById(R.id.T_VShowEmail);
+        T_VShowPhoneNum = findViewById(R.id.T_VShowPhoneNum);
+        progressBar = findViewById(R.id.progressBar);
+
+        authProfile = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = authProfile.getCurrentUser();
+
+        if (firebaseUser == null){
+            Toast.makeText(UserProfileActivity.this, "Something went wrong! User's details are not available at the moment", Toast.LENGTH_LONG).show();
+        } else {
+            progressBar.setVisibility(View.VISIBLE);
+            showUserProfile(firebaseUser);
+        }
+    }
+    private void showUserProfile(FirebaseUser firebaseUser){
+        String userID = firebaseUser.getUid();
+
+        //Extracting User Reference from Database for "Registered Users"
+        DatabaseReference referenceProfile = FirebaseDatabase.getInstance().getReference("Registered Users");
+        referenceProfile.child(userID).addListenerForSingleValueEvent(ValueEventListener);
     }
 }
