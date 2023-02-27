d5cc5e47e50bbf72e0e637ad64baa1ff57e87211
LianKhoury <khourylian1@gmail.com>
2023-02-27 09:56:29 +0200

    ss

--- app/src/main/java/com/example/liankhoury/ReadWriteUserDetails.java
+++ app/src/main/java/com/example/liankhoury/ReadWriteUserDetails.java
@@ -3,6 +3,8 @@ package com.example.liankhoury;
 public class ReadWriteUserDetails {
     public String phone;
 
+    public ReadWriteUserDetails(){};
+
     public ReadWriteUserDetails(String textPhone){
         this.phone = textPhone;
     }
