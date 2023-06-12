dadb5c6bb48693255245c9a45c61315a4c794c4e
LianKhoury <khourylian1@gmail.com>
2023-06-13 01:50:31 +0300

    ss

--- app/src/main/java/com/example/liankhoury/ReadWriteUserDetails.java
+++ app/src/main/java/com/example/liankhoury/ReadWriteUserDetails.java
@@ -1,9 +1,21 @@
 package com.example.liankhoury;
 
 public class ReadWriteUserDetails {
-    public String phone;
+    private String textPhoneNum;
+
+    public ReadWriteUserDetails() {
+        // Default constructor required for Firebase Realtime Database
+    }
 
     public ReadWriteUserDetails(String textPhoneNum) {
-        this.phone = textPhoneNum;
+        this.textPhoneNum = textPhoneNum;
+    }
+
+    public String getTextPhoneNum() {
+        return textPhoneNum;
+    }
+
+    public void setTextPhoneNum(String textPhoneNum) {
+        this.textPhoneNum = textPhoneNum;
     }
 }
